465858c114f3490b9ade21f81c43b24a70bec7f6
Edgar Zea <cesszea@icloud.com>
2020-05-26 14:08:01 -0500

    Updates

---
+++ precache-manifest.501bc332889c4ac8d344c33cd3df6cd4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61a80b7ca82cd88c294b5a921d6ea6ef",
+    "url": "/eamzea/index.html"
+  },
+  {
+    "revision": "2cc3de41d8389a1e24f6",
+    "url": "/eamzea/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "a2079a0099b4e61693e8",
+    "url": "/eamzea/static/css/main.a26f5a2d.chunk.css"
+  },
+  {
+    "revision": "2cc3de41d8389a1e24f6",
+    "url": "/eamzea/static/js/2.ac21d08e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/eamzea/static/js/2.ac21d08e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2079a0099b4e61693e8",
+    "url": "/eamzea/static/js/main.0e57faf1.chunk.js"
+  },
+  {
+    "revision": "0cd10d4793e2bc32d846",
+    "url": "/eamzea/static/js/runtime-main.86ed62ec.js"
+  }
+]);
